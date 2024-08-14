0070a4d7fe979b808460464919588ffa971f1506
shez <shezcode@gmail.com>
2024-08-14 19:58:51 +0200

    tabla usuarios

--- src/app/usuario/page.tsx
+++ src/app/usuario/page.tsx
@@ -1,24 +1,34 @@
-import { Separator } from "@/components/ui/separator";
+import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { fetchUsuarios } from "@/lib/fetch/fetchUsuarios";
+import Link from "next/link";
 
 const UserPage = async () => {
 
   const data = await fetchUsuarios();
 
-  const dataElements = data.map(usuario => {
-    return (
-      <div key={usuario.id_Usuario}>
-        <h1>{usuario.nombre} - {usuario.id_Usuario}</h1>
-        <h2>{usuario.email}</h2>
-        <h4>{usuario.departamento.nombre}</h4>
-        <Separator />
-      </div>
-    );
-  })
-
   return (
     <div>
-      {dataElements}
+      <Table>
+        <TableCaption className="mt-8">Lista de usuarios activos.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Nombre</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Fecha de alta</TableHead>
+              <TableHead className="text-right">Departamento</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {data.map((usuario) => (
+              <TableRow key={usuario.id_Usuario}>
+                <TableCell className="font-medium"><Link href={`/usuario/${usuario.id_Usuario}`}>{usuario.nombre}</Link></TableCell>
+                <TableCell>{usuario.email}</TableCell>
+                <TableCell>{usuario.fe_alta}</TableCell>
+                <TableCell className="text-right">{usuario.departamento.nombre}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+      </Table>
     </div>
   );
 }
